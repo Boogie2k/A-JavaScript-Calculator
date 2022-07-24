bac5ea4360a5efda8dae8b32db17d62acb5663de
Boogie2k <oghosabenjamin@gmail.com>
2022-07-24 20:21:20 +0100

    A javascript calculator

---
+++ calca.js
@@ -0,0 +1,94 @@
+
+alert("please follow my twitter account")
+
+  function zero() {
+  	document.getElementById("screen").value  = 0;}
+			// body...
+	 
+
+		
+ 
+
+ 
+
+ 
+
+	 	 function one()  {  document.getElementById("screen").value += 1;
+			// body...
+	 	}
+ 
+	
+	 	function two() {  document.getElementById("screen").value += 2;
+	 		
+			// body...
+	 	}
+
+	 	function three(){ document.getElementById("screen").value += 3;}
+
+
+	 	 function four()  {  document.getElementById("screen").value += 4;
+			// body...
+	 	}
+ 
+	
+	 	function five() {  document.getElementById("screen").value += 5;
+	 		
+			// body...
+	 	}
+
+	 	function six() { document.getElementById("screen").value += 6;}
+	 	
+	 	function seven() {document.getElementById("screen").value += 7;
+			// body...
+	 	}
+
+	 	 function eight()  {  document.getElementById("screen").value +=8;
+			// body...
+	 	}
+ 
+	
+	 	function nine() {  document.getElementById("screen").value += 9;
+	 		
+			// body...
+	 	}
+
+	 	 
+	 	 
+	 	function add() {document.getElementById("screen").value += "+";
+			// body...
+	 	}
+
+
+	 	function times() {document.getElementById("screen").value += "*";
+			// body...
+	 	}
+
+
+	 	function minus() {document.getElementById("screen").value += "-";
+			// body...
+	 	}
+
+
+	 	function divide() {document.getElementById("screen").value +="/";
+			// body...
+	 	}
+
+
+	 	function point() {document.getElementById("screen").value +=".";
+			// body...
+	 	}
+
+	 	function equal() {
+
+         
+
+
+        document.getElementById("screen").value  =   eval(document.getElementById("screen").value)
+			// body...
+	 	}
+
+	 		function del() {document.getElementById("screen").value = document.getElementById("screen").value.slice(0, -1) 
+			// body...
+	 	}
+
+function sci() {document.getElementById("screen").value  =" "} 
